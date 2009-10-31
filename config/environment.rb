5220348252082681ae18efab00071448317aafdb
Tom-Eric <ik@tom-eric.info>
2009-10-31 13:58:05 +0100

    Remove stuff we don't use!

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml",        :source => "http://gemcutter.org"
-  config.gem "searchlogic", :source => "http://gemcutter.org"
-  config.gem "formtastic",  :source => "http://gemcutter.org"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
