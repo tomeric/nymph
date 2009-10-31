e456df9e1ed95ef48f1f9c5a6f752e7c12f19df2
Rik Tonnard <Rik@133.220.092.145.hva.nl>
2009-10-31 13:16:14 +0100

    Added rss methods

--- app/models/nymph/gem.rb
+++ app/models/nymph/gem.rb
@@ -1,9 +1,12 @@
+require 'open-uri'
+require 'nokogiri'
+
 module Nymph
   class Gem
     @@configuration = ::Gem.configuration.send(:hash).dup.freeze
     cattr_accessor :configuration
     
-    attr_accessor :name, :current_version, :dependencies, :requirements
+    attr_accessor :name, :rss_feeds, :current_version, :dependencies, :requirements
     
     def initialize(options = {})
       options.each do |key, value|
@@ -24,7 +27,19 @@ module Nymph
     def self.parse_gem(gem)
       Gem.new(:name            => gem.name,
               :current_version => gem.respond_to?(:version) ? gem.version : nil,
-              :dependencies    => Dependency.parse(gem.dependencies))      
+              :dependencies    => Dependency.parse(gem.dependencies),
+              :rss_feeds       => get_rss_feeds(gem))      
+    end
+    
+    def self.get_rss_feeds(gem)
+      return []# unless gem.homepage
+      
+      doc = Nokogiri::HTML(open(gem.homepage))
+      feeds = []
+      
+      doc.xpath('//link[@type="application/atom+xml" or @type="application/rss+xml"]').each do |atom|
+        feeds << { :title => atom.attributes["title"].value, :href => atom.attributes["href"].value }
+      end
     end
   end
 end
